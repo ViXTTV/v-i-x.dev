8db4f9516a4403ab30582ac729ef65d1e74eca44
ViX_TV <104933156+ViXTTV@users.noreply.github.com>
2022-10-27 19:22:54 +0200

    Add files via upload

---
+++ AnimateWebsite.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    document.getElementById('AnimatedText1').style.display = 'none';
+}
+
+function AnimateWebsite() {
+    document.getElementById('NonAnimatedText1').style.display="none"
+    document.getElementById('AnimateButton').style.display="none"
+}
+
+function AnimateWebsite2() {
+    document.getElementById('AnimatedText1').style.display="block"
+    document.getElementById('AnimateButton').style.display="none"
+}
