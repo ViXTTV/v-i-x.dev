6bdfa2cc5a4c452ee0c22e7f1e2ca191cd453d81
ViX_TV <104933156+ViXTTV@users.noreply.github.com>
2022-11-06 15:38:33 +0100

    Update AnimateWebsite.js

--- AnimateWebsite.js
+++ AnimateWebsite.js
@@ -1,7 +1,3 @@
-window.onload = function() {
-    document.getElementById('AnimatedText1').style.display = 'none';
-}
-
 function playAll() {
     document.querySelector('.container').classList.add('playing');
     const audios = document.querySelectorAll('audio');
@@ -18,4 +14,9 @@ function AnimateWebsite() {
 function AnimateWebsite2() {
     document.getElementById('AnimatedText1').style.display="block"
     document.getElementById('AnimateButton').style.display="none"
+}
+
+function ShowTextNow() {
+    document.getElementById('invisible').style.display="block"
+    document.getElementById('visible').style.display="none"
 }
